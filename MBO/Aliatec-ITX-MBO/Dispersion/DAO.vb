7281fe7a3c8e44a31538d31a0de465ecc7870d46
AliaTec <vbarragan@aliatec.com.mx>
2016-03-28 11:30:46 -0600

    Inicio

---
+++ MBO/Aliatec-ITX-MBO/Dispersion/DAO.vb
@@ -0,0 +1,141 @@
+﻿Imports Intelexion.Framework.Model
+Imports System.Data
+Imports Intelexion.Nomina
+Imports System.Web
+Imports System.Collections
+Imports System.Collections.Specialized
+Imports Intelexion.Framework.Controller
+Imports Intelexion.Framework.View
+Imports System.IO
+Imports System.Data.SqlClient
+
+Public Class DAO
+    Inherits Intelexion.Framework.Model.DAO
+
+
+    Private Const Contabilidad As String = "sp_Reporte_Contabilidad '@IdRazonSocial','@IdTipoNominaAsig','@IdTipoNominaProc','@Anio','@Periodo','@UID','@LID','@idAccion'"
+    Private Const GravaExenMen As String = "sp_NomGraExeMensual '@IdRazonSocial','@IdTipoNominaAsig', '@folioDesde', '@folioHasta', '@UID', '@LID', '@idAccion'"
+    Private Const Contabilidad_Mensual As String = "sp_Reporte_Contabilidad_Mensual '@IdRazonSocial','@IdTipoNominaAsig','@IdTipoNominaProc','@folioDesde','@folioHasta','@UID','@LID','@idAccion'"
+    Private Const ISN As String = "sp_Reporte_ImpuestoEstatal '@IdRazonSocial','@IdTipoNominaAsig','@folioDesde','@folioHasta','@UID','@LID','@idAccion'"
+    Private Const ReporteMenLM As String = "sp_ReporteMensualListadoMaestro '@IdRazonSocial','@IdTipoNominaAsig','@IdTipoNominaProc','@folioDesde','@folioHasta','@UID','@LID','@idAccion'"
+    Private Const LayoutDispersionHSBC As String = "sp_Reporte_LayoutDispersionHSBC_ACH '@IdRazonSocial','@IdTipoNominaAsig','@IdTipoNominaProc','@Anio','@Periodo','@UID','@LID','@idAccion'"
+
+
+
+    Public Sub New(ByVal DataConnection As SQLDataConnection)
+        MyBase.New(DataConnection)
+    End Sub
+    Public Function Layout(ByVal ReportesProceso As EntitiesITX.ReportesProceso, ByVal opL As String) As DataSet
+        Dim ds As New DataSet
+        Dim resultado As Boolean
+        Dim comandstr As String
+        Try
+            Select Case opL
+
+
+                Case "Contabilidad"
+                    comandstr = Contabilidad
+                    resultado = Me.ExecuteQuery(comandstr, ds, ReportesProceso)
+                    Return ds
+
+                Case "Contabilidad_Mensual"
+                    comandstr = Contabilidad_Mensual
+                    resultado = Me.ExecuteQuery(comandstr, ds, ReportesProceso)
+                    Return ds
+
+                Case "ISN"
+                    comandstr = ISN
+                    resultado = Me.ExecuteQuery(comandstr, ds, ReportesProceso)
+                    Return ds
+
+
+                Case "GravaExenMen"
+                    comandstr = GravaExenMen
+                    resultado = Me.ExecuteQuery(comandstr, ds, ReportesProceso)
+                    Return ds
+
+                Case "ReporteMenLM"
+                    comandstr = ReporteMenLM
+                    resultado = Me.ExecuteQuery(comandstr, ds, ReportesProceso)
+                    Return ds
+
+            End Select
+        Catch e As Exception
+        End Try
+        Return ds
+    End Function
+
+    Public Function LayoutTxt(ByVal ReportesProceso As EntitiesITX.ReportesProceso, ByVal opL As String, ByVal context As System.Web.HttpContext) As String
+        Dim ds As New DataSet
+        Dim sFile As String
+        Dim sPathApp As String = Intelexion.Framework.ApplicationConfiguration.ConfigurationSettings.GetConfigurationSettings("ApplicationPath")
+        Dim sPathArchivosTemp As String = Intelexion.Framework.ApplicationConfiguration.ConfigurationSettings.GetConfigurationSettings("ArchivosTemporales")
+        'Dim ruta As String
+        Try
+            Select Case opL
+
+
+                Case "LayoutDispersionHSBC"
+                    Dim results As ResultCollection
+                    Dim objLayoutDispersion As Entities.LayoutDispersion
+                    Dim dTotalImporte As Decimal
+                    Dim sCadena As String
+                    Dim i As Integer
+                    results = New ResultCollection
+                    ReportesProceso.tipoArchivo = 0
+                    objLayoutDispersion = New Entities.LayoutDispersion
+                    objLayoutDispersion.IdRazonSocial = context.Session("IdRazonSocial")
+                    objLayoutDispersion.UID = context.Session("UID")
+                    objLayoutDispersion.LID = context.Session("LID")
+                    objLayoutDispersion.idAccion = context.Items.Item("idAccion")
+                    Dim UserId As String
+                    UserId = ReportesProceso.UID.ToString
+                    UserId = UserId.Replace("/", "")
+                    UserId = UserId.Replace("\", "")
+                    UserId = UserId.Replace("%", "")
+                    UserId = UserId.Replace("_", "")
+                    UserId = UserId.Replace("-", "")
+                    sFile = "\LayoutDispersionHSBC" + ReportesProceso.IdRazonSocial.ToString + UserId + Date.Now.Second.ToString + ".txt"
+
+                    results.getEntitiesFromDataReader(objLayoutDispersion, Me.ReporteDispersionHSBC(ReportesProceso))
+                    dTotalImporte = 0
+                    If results.Count > 0 Then
+                        Dim sb As New FileStream(context.Server.MapPath(sPathApp + sPathArchivosTemp) + sFile, FileMode.Create)
+                        Dim sw As New StreamWriter(sb)
+
+                        For i = 0 To results.Count - 1
+                            sCadena = results(i).centroCosto
+                            sw.WriteLine(sCadena)
+                        Next i
+
+                        sw.Close()
+
+                    End If
+
+                    Return sPathApp + sPathArchivosTemp + sFile
+
+
+            End Select
+        Catch e As Exception
+        End Try
+        Return ""
+    End Function
+
+    Public Function ReporteDispersionHSBC(ByVal ReportesProceso As EntitiesITX.ReportesProceso) As SqlDataReader
+        Dim data As SqlDataReader = Nothing
+        Dim resultado As Boolean
+        Dim comandstr As String
+        Try
+            comandstr = LayoutDispersionHSBC
+            resultado = Me.ExecuteQuery(comandstr, data, ReportesProceso)
+            Return data
+        Catch e As Exception
+        End Try
+        Return data
+    End Function
+
+
+
+
+
+End Class
---
+++ MBO/Aliatec-ITX-MBO/Dispersion/Items.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Data
+
+Public Class Items
+    Inherits DataTable
+
+    Public Sub New()
+        Me.Columns.Add("Label")
+        Me.Columns.Add("Accion")
+        Me.Columns.Add("Case")
+
+        Dim newRow As DataRow
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100015
+        'accion
+        newRow.Item("Accion") = 100015
+        'case del aspx
+        newRow.Item("Case") = 100015
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100016
+        'accion
+        newRow.Item("Accion") = 100016
+        'case del aspx
+        newRow.Item("Case") = 100016
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100020
+        'accion
+        newRow.Item("Accion") = 100020
+        'case del aspx
+        newRow.Item("Case") = 100020
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100021
+        'accion
+        newRow.Item("Accion") = 100021
+        'case del aspx
+        newRow.Item("Case") = 100021
+        Me.Rows.Add(newRow)
+
+        newRow = Me.NewRow()
+        newRow.Item("Label") = 100022
+        'accion
+        newRow.Item("Accion") = 100022
+        'case del aspx
+        newRow.Item("Case") = 100022
+        Me.Rows.Add(newRow)
+
+    End Sub
+End Class
